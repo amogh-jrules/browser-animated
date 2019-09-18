76ef3d1cb8016ec02ae005ba7af4b8ee7b9ab6b1
Amogh J <amoghjahagirdargsf@gmail.com>
2019-09-18 18:26:27 +0530

    Swipe to close tab implemented

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,8 +1,13 @@
 import React, { useMemo, useRef } from "react";
 import { StyleSheet, View, Dimensions, WebView, Text } from "react-native";
 import Constants from "expo-constants";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { snapPoint, onGestureEvent, approximates } from "react-native-redash";
 import { TabModel, OVERVIEW } from "./Model";
+import Animated from "react-native-reanimated";
+import spring from "./Spring";
 
+const { Value, cond, useCode, abs, call } = Animated;
 const { width } = Dimensions.get("window");
 const EXTREMITY = width * 1.1;
 const snapPoints = [-EXTREMITY, 0, EXTREMITY];
@@ -29,8 +34,23 @@ interface ContentProps {
 
 export default ({ tab: { uri, id: title }, selectedTab }: ContentProps) => {
   const offset = selectedTab === OVERVIEW ? 0 : Constants.statusBarHeight;
+  const {gestureEvent,translateX} = useMemo(()=>{
+    const translationX = new Value(0);
+    const velocityX = new Value(0);
+    const state = new Value(State.UNDETERMINED);
+    return {
+      gestureEvent : onGestureEvent({
+        translationX,
+        velocityX,
+        state,
+
+      }),
+      translateX : spring(translationX,state,snapPoint(translationX, velocityX, snapPoints))
+    }
+  })
   return (
-    <View style={styles.container}>
+    <PanGestureHandler {...gestureEvent}>
+    <Animated.View style={[styles.container,{transform :[{translateX}] }]}>
       <View
         style={{
           paddingTop: offset,
@@ -45,6 +65,7 @@ export default ({ tab: { uri, id: title }, selectedTab }: ContentProps) => {
         <WebView source={{ uri }} style={styles.webView} />
         <View style={StyleSheet.absoluteFill} />
       </View>
-    </View>
+    </Animated.View>
+  </PanGestureHandler>
   );
 };
